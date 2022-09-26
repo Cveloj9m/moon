b79ce1830dada18cc2d74ea86c25b2f9c44fcc80
Cveloj9m <50126324+Cveloj9m@users.noreply.github.com>
2022-09-26 23:31:57 +0800

    Initial commit

---
+++ src/components/moon/MoonSection.vue
@@ -0,0 +1,45 @@
+<template>
+    <input type="radio" name="1" :id="group" :checked="active">
+    <label class="card" :for="group" :id="id">
+        <div>6666666666</div>
+    </label>
+</template>
+
+<script>
+    import { useCounterStore } from '../stores/counter'
+    export default {
+        name: "moon section",
+        setup() {
+            const counterStore = useCounterStore()
+            return { counterStore }
+        },
+        props: {
+            'id': {type: Number, required: true},
+            'active': {type: Boolean, default: false},
+            'order': {type: Number, default: 0},
+            'group': {type: String, required: true},
+        },
+        data() {
+            return {
+
+            }
+        },
+        mounted(){
+
+        },
+        components: {
+
+        },
+        methods: {
+
+        },
+        created() {
+
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+
+
+</style>
